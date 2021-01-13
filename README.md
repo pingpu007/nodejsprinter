e212993b82a664f2409a57a1200f5c707b830957
sorawit sirimaleewattana <sorawitsirimaleewattana@sorawits-MacBook-Pro.local>
2021-01-13 10:29:45 +0700

    Prove concept ESC/POS

--- README.md
+++ README.md
@@ -3,6 +3,13 @@ ESC/POS Printer driver for Node.js
 
 ## Installation
 
+### Dependency
+```bash
+"node": v14.15.4
+"npm": v6.14.10
+"escpos": ^3.0.0-alpha.6,
+"escpos-usb": ^3.0.0-alpha.4
+```
 ### git
 ```bash
 $ git clone https://github.com/SorawitSiri/posprinter-js
---
+++ main.js
@@ -0,0 +1,40 @@
+console.log("STEP #1")
+const escpos = require('escpos');
+console.log("STEP #2")
+
+// install escpos-usb adapter module manually
+escpos.USB = require('escpos-usb');
+console.log("STEP #3")
+
+const device  = new escpos.USB();
+// const device  = new escpos.Network('localhost');
+// const device  = new escpos.Serial('/dev/usb/lp0');
+
+const options = { encoding: "GB18030" /* default */ }
+// encoding is optional
+
+const printer = new escpos.Printer(device, options);
+
+device.open(function(error){
+  printer
+  .font('a')
+  .align('ct')
+  .style('bu')
+  .size(1, 1)
+  .text('The quick brown fox jumps over the lazy dog')
+  .text('敏捷的棕色狐狸跳过懒狗')
+  .barcode('1234567', 'EAN8')
+  .table(["One", "Two", "Three"])
+  .tableCustom(
+    [
+      { text:"Left", align:"LEFT", width:0.33, style: 'B' },
+      { text:"Center", align:"CENTER", width:0.33},
+      { text:"Right", align:"RIGHT", width:0.33 }
+    ],
+    { encoding: 'cp857', size: [1, 1] } // Optional
+  )
+  .qrimage('https://github.com/song940/node-escpos', function(err){
+    this.cut();
+    this.close();
+  });
+});
